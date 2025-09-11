2cc31976d657cad77cb7980bac918b956d164565
x2RunE <zeenogenesis.space@proton.me>
2025-09-11 21:26:48 +0700

    Update radiant.lua

--- radiant.lua
+++ radiant.lua
@@ -1,2 +1,2 @@
 -- This file was protected using Luraph Obfuscator v14.4.2 [https://lura.ph/]
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/cad24e997cb397c5"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/11efe09560491f96"))()
