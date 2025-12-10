04b451cef1c42439b0921ea115ae76f35e2d0410
x2RunE <zeenogenesis.space@proton.me>
2025-12-10 23:25:30 +0700

    Update radiant.lua

--- radiant.lua
+++ radiant.lua
@@ -5,9 +5,9 @@ local place_id = {
     [7465136166] = "Grand Piece Online",
     [3978370137] = "Grand Piece Online",
     [6811831486] = 'Grand Piece Online',
-    [2753915549] = 'Blox Fruits',
-    [4442272183] = 'Blox Fruits',
-    [7449423635] = 'Blox Fruits'
+    [85211729168715] = 'Blox Fruits',
+    [79091703265657] = 'Blox Fruits',
+    [100117331123089] = 'Blox Fruits'
 }
 if place_id[game.PlaceId] == "Grand Piece Online" then
     if getgenv().Kaitun then
@@ -15,4 +15,6 @@ if place_id[game.PlaceId] == "Grand Piece Online" then
     else
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7ff3bd5d1081c93c8eec4788572f2c6c.lua"))() 
     end
+elseif place_id[game.PlaceId] == "Blox Fruits" then
+    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/1e48400962a52bef"))()
 end
