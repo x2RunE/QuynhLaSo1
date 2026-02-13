0ab91c944eb42fa20aded3bc9bceafccb960edbe
x2RunE <zeenogenesis.space@proton.me>
2026-02-13 23:18:56 +0700

    Update fps-booster.lua

--- data/fps-booster.lua
+++ data/fps-booster.lua
@@ -1,27 +1,32 @@
 local g = game
 local s = settings()
+local lighting = g:GetService("Lighting")
 pcall(function()
     s.Rendering.QualityLevel = Enum.QualityLevel.Level01
 end)
-for _,v in pairs(g:GetDescendants()) do
-    if v:IsA("BasePart") then
-        v.Material = Enum.Material.Plastic
-        v.Reflectance = 0
-    elseif v:IsA("Decal") or v:IsA("Texture") then
-        v:Destroy()
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-        v.Enabled = false
-    elseif v:IsA("Explosion") then
-        v.BlastPressure = 0
-        v.BlastRadius = 0
+local function optimize(obj)
+    if obj:IsA("BasePart") then
+        obj.Material = Enum.Material.Plastic
+        obj.Reflectance = 0
+    elseif obj:IsA("Decal") or obj:IsA("Texture") then
+        obj:Destroy()
+    elseif obj:IsA("ParticleEmitter") or obj:IsA("Trail") then
+        obj.Enabled = false
+    elseif obj:IsA("Explosion") then
+        obj.BlastPressure = 0
+        obj.BlastRadius = 0
     end
 end
-lighting = g:GetService("Lighting")
+for _,v in pairs(g:GetDescendants()) do
+    optimize(v)
+end
+g.DescendantAdded:Connect(optimize)
 lighting.GlobalShadows = false
 lighting.FogEnd = 1e9
 lighting.Brightness = 0
 lighting.EnvironmentDiffuseScale = 0
 lighting.EnvironmentSpecularScale = 0
+
 for _,v in pairs(lighting:GetChildren()) do
     if v:IsA("PostEffect") then
         v.Enabled = false
