9ff21ebcc1786974a9ad7f3b7708a3d03da48ba3
x2RunE <zeenogenesis.space@proton.me>
2026-06-03 03:31:43 +0700

    Update rcn_script.lua

--- rcn_script.lua
+++ rcn_script.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 local plr = game.Players.LocalPlayer
 while true do
-  writefile(plr.Name.."_IMT_RCN.txt",tostring(math.floor(tick())))
+  writefile(game.Players.LocalPlayer.Name.."_IMT_RCN.txt",tostring(math.floor(tick()) - 25200))
   task.wait(5)
 end
